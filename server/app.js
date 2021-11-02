bc464884458d5d96937ba29a236d118c85b5b2c8
amandaheideman <hedieman.amanda@gmail.com>
2021-11-02 11:45:27 +0100

    CORS fix

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const listRouter = require('./routes/listRouter');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const path = require("path");
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 const uri = process.env.ATLAS_URI;
+
+app.use(cors());
+//app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 mongoose
   .connect(uri, {
