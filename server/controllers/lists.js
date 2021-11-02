dc3cf990b689ed1c15d84155f1ab554d98e885c7
amandaheideman <hedieman.amanda@gmail.com>
2021-11-02 23:48:28 +0100

    set up controller

--- server/controllers/lists.js
+++ server/controllers/lists.js
@@ -1,14 +1,25 @@
-/* const lists = require("../models/ListModel");
+const Lists = require("../models/ListModel");
 
 const getAllLists = async (req, res) => {
   try {
-    const allLists = await lists.find();
+    const allLists = await Lists.find();
     res.status(200).json(allLists);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
 
+const getDetailList = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const list = await Lists.findById(id);
+    res.status(200).json(list);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
 module.exports = {
-  getAllLists
-}; */
+  getAllLists,
+  getDetailList
+};
--- server/routes/listRouter.js
+++ server/routes/listRouter.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const router = express.Router();
-//const { getAllLists } = require('../controllers/lists');
-let List = require("../models/ListModel");
+const { getAllLists, getDetailList } = require('../controllers/lists');
+//let List = require("../models/ListModel");
 
-//message kommer vara array med objekt, med id och titel på listorna, hämtas från databas
-//const message = getAllLists();
-//console.log(message);
 /* 
-router.get('/', (req, res) => {
-  res.json(message);
-});
- */
-
-
-
-
 router.route('/').get((req, res) => {   //vår route, endpoint som tar emot get på /user. 
   List.find()    //kommer hämta alla users från vår databas, find returnerar ett promise 
     .then(lists => res.json(lists))   //returnerar users 
     .catch(err => res.status(400).json('Error: ' + err));
-});
+}); */
 
-/* 
-const getAllLists = async (req, res) => {
-  try {
-    const allLists = await lists.find();
-    res.status(200).json(allLists);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
- */
-/* module.exports = {
-  getAllLists
-};
- */
-
-
+router.get("/", getAllLists);
+router.get("/:id", getDetailList);
 
 module.exports = router;
 
