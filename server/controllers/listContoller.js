0a52bdf3e0ccbf372dce53b634b4e1adde00aae0
Mathilde Åkerlind <mathilde.akerlind@gmail.com>
2021-11-04 13:44:56 +0100

    add listItem to db from client

--- server/controllers/listContoller.js
+++ server/controllers/listContoller.js
@@ -1,8 +1,8 @@
-const Lists = require("../models/ListModel");
+const List = require("../models/ListModel");
 
 const getAllLists = async (req, res) => {
   try {
-    const allLists = await Lists.find();
+    const allLists = await List.find();
     res.status(200).json(allLists);
   } catch (err) {
     res.status(404).json({ message: err.message });
@@ -12,7 +12,7 @@ const getAllLists = async (req, res) => {
 const getDetailList = async (req, res) => {
   const id = req.params.id;
   try {
-    const list = await Lists.findById(id);
+    const list = await List.findById(id);
     res.status(200).json(list);
   } catch (err) {
     res.status(404).json({ message: err.message });
@@ -20,21 +20,21 @@ const getDetailList = async (req, res) => {
 };
 
 const updateList = async (req, res) => {
-  const id = req.params.id;
-  //const { listItem } = req.body;
-  console.log("hallåå");
-  try {
-    Lists.findByIdAndUpdate(
-      id,
-      {
-        listItem: "newListItem"
-      }
-    )
-    console.log("???");
-  }
-  catch (err) {
-    res.status(404).json({ message: err.message });
-  }
+  const newListItem = req.body.listItem;
+  List.findOne({ _id: req.params.id }, (err, list) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      list.listItem.push(newListItem);
+      list.save(error => {
+        if (error) return console.log(error)
+        //saved
+        return res.status(200).json({
+          title: 'success'
+        });
+      });
+    }
+  })
 }
 
 
--- server/routes/listRouter.js
+++ server/routes/listRouter.js
@@ -7,7 +7,7 @@ const List = require('../models/ListModel')
 
 router.get("/", getAllLists);
 router.get("/:id", getDetailList);
-//router.post("/:id", updateList);
+router.post("/:id", updateList);
 
 //router.post("/", addList);
 
@@ -30,46 +30,6 @@ router.route("/").post((req, res) => {
     })
   })
 });
-
-
-router.route('/:id').post((req, res) => {
-  // now we know token is valid
-  List.findOne({ _id: req.params.id }, (err, list) => {
-    if (err) {
-      return console.log(err);
-    } else {
-      console.log('added to db?')
-
-      list.listItem.push('A NEW LIST ITEM');
-
-      list.save(error => {
-        if (error) return console.log(error)
-        //saved
-        return res.status(200).json({
-          title: 'success',
-
-        });
-      });
-    }
-  })
-})
-
-/* router.route("/:id").post((req, res) => {
-  const id = req.params._id;
-  const { listItem } = req.body;
-  console.log("update item")
-  try {
-    List.findByIdAndUpdate(
-      id,
-      {
-        $push: { listItem: listItem }
-      }
-    )
-      .catch((err) => res.status(500).json({ msg: err.message }));
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-}) */
 
 
 module.exports = router;
