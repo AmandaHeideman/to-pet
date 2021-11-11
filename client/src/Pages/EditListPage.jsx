c17f7d244da214a41d683e534e7fa33caaa73634
Mathilde Åkerlind <mathilde.akerlind@gmail.com>
2021-11-11 16:49:30 +0100

    changing mongod db variabel

--- client/src/Pages/EditListPage.jsx
+++ client/src/Pages/EditListPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const url = axios.create({
-  baseURL: process.env.API_KEY || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_SERVER_URL
 });
 
 const EditListPage = (props) => {
